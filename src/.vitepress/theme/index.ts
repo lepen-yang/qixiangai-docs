191f3349a07809feec34f1ed7bf7f796678eabc4
liangx <1474862431@qq.com>
2026-08-05 00:52:54 +0800

    feat

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,9 +1,8 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-
+import posthog from "posthog-js";
 import HeroDownloadDropdown from "./components/HeroDownloadDropdown.vue";
-
 import "./custom.css";
 
 export default {
@@ -13,7 +12,14 @@ export default {
       "home-hero-actions-after": () => h(HeroDownloadDropdown),
     });
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
     app.component("HeroDownloadDropdown", HeroDownloadDropdown);
+    if (import.meta.env.PROD) {
+      posthog.init(import.meta.env.VITE_POSTHOG_KEY);
+      // 路由切换上报pageview
+      router.onAfterRouteChange = () => {
+        posthog.capture("$pageview");
+      };
+    }
   },
 } satisfies Theme;
